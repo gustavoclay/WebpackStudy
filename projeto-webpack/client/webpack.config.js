968fb986480689767923f5ad08916e495b7acf12
Gustavo Clay <gustavo.lima@eigmercados.com.br>
2018-12-28 15:53:32 -0200

    Curso Webpack: Manipulando módulos na sua webapp Aula: 01 Utilizando o npm para gerenciar dependências front-end

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     entry: './app-src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
     },
     module: {
 
